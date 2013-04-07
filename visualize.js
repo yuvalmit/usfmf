ec5c60ac22636104b57429106527c74bcadf420d
Idan Gazit <idan@gazit.me>
2013-04-07 06:30:15 +0300

    Working visualization

--- visualize.js
+++ visualize.js
@@ -1,18 +1,51 @@
 var margin = {top: 20, right: 20, bottom: 30, left: 50},
         width = 960 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
-    
+        height = 4550 - margin.top - margin.bottom,
+        y = 25; // bar height
+
+var funding_max = 13648194056,
+    population_max = 1347565324,
+    bubble_max_radius = 100;
+
 var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.csv("data.csv", function(error, data) {
+
+var x = d3.scale.linear()
+        .domain([0, funding_max])
+        .range([0, width]);
+
+var r = d3.scale.linear()
+        .domain([0, population_max])
+        .range([10, bubble_max_radius]);
+
+var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+d3.json("data.json", function(error, data) {
   console.log("data loaded!");
-  // Dummy operation to show that d3 is working.
-  svg.append("circle")
-    .attr("cx", 100)
-    .attr("cy", 100)
-    .attr("r", 20);
+
+  // svg.append("g")
+  //     .attr("class", "x axis")
+  //     .call(xAxis);
+
+  var countries = svg.selectAll(".country")
+    .data(data)
+    .enter().append("g")
+    .attr("class", "country")
+    .attr("transform", function(d, i) { return "translate(0, " + i * y + ")"; });
+
+  countries.append("text")
+    .style("text-anchor", "left")
+    .text(function(d) { return d.name; });
+
+  countries.append("circle")
+    .attr("cy", -y/4)
+    .attr("cx", function(d) { return x(d.annual_aid[61]); })
+    .attr("r", function(d) { return r(d.population[61]); });
+
 });
